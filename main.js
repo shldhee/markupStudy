be21a2e16a8f87cb2366b77e4083e2ab7b6295a5
LEEDUCKHEE <shldhee@yahoo.co.kr>
2017-12-04 18:02:45 +0900

    working slider wrap

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).on('ready', function() {
+  $('.slider__list').slick({
+    // autoplay: true,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 100,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    }]
+  });
+});
